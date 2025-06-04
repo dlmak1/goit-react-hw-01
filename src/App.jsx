ad15c3d1246b730f8d080a761c3ab0a55a987596
Dmytro <dlmakseverin@gmail.com>
2025-06-04 15:30:52 +0300

    task 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 
 import Profile from './components/Profile/Profile';
+import FriendList from './components/FriendList/FriendList';
 
 import userData from './data/userData.json';
+import friends from './data/friends.json';
 
 const App = () => {
   return (
     <div className="container">
       <Profile {...userData} />
+      <FriendList friends={friends} />
     </div>
   );
 };
